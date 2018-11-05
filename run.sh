7cf0bd7443b55cb4b2a61c9079637c38e9a8c5ec
qiuxin2012 <qiuxin2012cs@gmail.com>
2018-11-05 09:54:34 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ mkdir logs
 for i in $(seq 1 $times)
 do
   seed=`date +%s`
-  echo "sh run_and_time.sh 22 $seed"
-  sh run_and_time.sh 22 $seed > logs/seed$seed.log
+  echo "sh run_and_time.sh 28 $seed"
+  sh run_and_time.sh 28 $seed > logs/seed$seed.log
 done
